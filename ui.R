101174d79fd99f3b0771559fafe174535091d43b
emilygarat <garatemi@uw.edu>
2022-05-29 20:09:22 -0700

    created initial shiny page. included tabPanels for Introduction, Chart 1, Chart 2, and Chart 3. changed shinytheme to flatly.

--- ui.R
+++ ui.R
@@ -9,16 +9,32 @@ intro_tab <- tabPanel(
   )
 )
 
-chart1_tab
+chart1_tab <- tabPanel(
+  "Chart 1",
+  fluidPage(
+    p("Chart 1 will go here.")
+  )
+)
 
-chart2_tab
+chart2_tab <- tabPanel(
+  "Chart 2",
+  fluidPage(
+    p("Chart 2 will go here.")
+  )
+)
 
-chart3_tab
+chart3_tab <- tabPanel(
+  "Chart 3",
+  fluidPage(
+    p("Chart 3 will go here.")
+  )
+)
 
 
 ui <- navbarPage(
   "2020 NIHS Data",
   theme = shinytheme("flatly"),
+  intro_tab,
   chart1_tab,
   chart2_tab,
   chart3_tab
