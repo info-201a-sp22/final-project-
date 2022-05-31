90e6cc13b97e9118670ae434fc92b461e60a4409
emilygarat <garatemi@uw.edu>
2022-05-31 16:19:32 -0700

    had accidentally pasted a4 code - removed and cleaned up

--- server.R
+++ server.R
@@ -5,21 +5,20 @@ library(tidyverse)
 library(markdown)
 options(scipen = 999)
 
-data <- read.csv("", stringsAsFactors = FALSE)
+
 Emily_Dataset <- read.csv("Emily_SmallDataset.csv")
 
-final_data <- 
-
-server <- function(input, output) {
-  
-  output$plot <- renderPlotly({
-    
-  groupedbar <- Emily_Dataset %>% 
-    filter(Country %in% input$Selections)
-  
-    interactive_plot <- ggplot(data = ) +
-      geom_point(mapping = aes(x = , y = , color = )) +
-      ggtitle("")
-    return(interactive_plot)
-  })
-}
+# final_data <- 
+# 
+# server <- function(input, output) {
+#   
+#   output$plot <- renderPlotly({
+#     
+# 
+#   
+#     interactive_plot <- ggplot(data = ) +
+#       geom_point(mapping = aes(x = , y = , color = )) +
+#       ggtitle("")
+#     return(interactive_plot)
+#   })
+# }
