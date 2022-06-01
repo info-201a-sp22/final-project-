7e390aebd87e8cce9bd4acd926e99424c13a90da
emilygarat <garatemi@uw.edu>
2022-05-31 22:32:24 -0700

    em and z's updated code

--- server.R
+++ server.R
@@ -1,31 +1,55 @@
-library(ggplot2)
-library(plotly)
-library(dplyr)
-library(tidyverse)
-library(markdown)
 library(shiny)
+library(shinythemes)
+library(markdown)
+library(bslib) 
+library(htmlwidgets) 
+library(DT)
+library(plotly) 
+library(ggplot2) 
+library(dplyr)
+library(hrbrthemes)
+library(scales)
 options(scipen = 999)
 
-Emily_Dataset <- read.csv("Emily_SmallDataset.csv")
-zchart <- read.csv("zchart.csv")
+chart1 <- read.csv("zchart.csv")
+chart2 <- read.csv("emchart.csv")
+chart3 <- read.csv("chart3.csv")
 
-  output$pie <- renderPlotly ({
+server <- function(input, output) {
+  
+  output$pie <- renderPlotly({
     
-    Interactive_pie_chart<- zchart %>% 
+    interactive_pie_chart <- chart1 %>% 
       filter(Mental_Health == input$count) 
     
-    Pie_Chart <- ggplot(zchart, aes(x="", y=count, fill= Mental_Health)) +
+    Pie_Chart <- ggplot(chart1, aes(x="", y=count, fill= Mental_Health)) +
       geom_bar(stat="identity", width=1, color="white") +
       geom_label(aes(label = count),
                  position = position_stack(vjust = 0.5),
                  show.legend = FALSE) +
       coord_polar(theta = "y", start=0) +
       theme_void() +
-      labs(title = 'Prevelance of Anxiety Impacting Social Functioning and Mobility', fill = "Mental Health Co-Varients") +
+      labs(title = 'Prevalence of Anxiety Impacting Social Functioning and Mobility', fill = "Mental Health Co-Variants") +
       scale_fill_discrete(labels = c("Difficulty Doing Errands", "Difficulty Socially Particpating", "Ever Had Anxiety"))
     return(Pie_Chart)   
-  )
-   
+  })
+  
+  output$plot2 <- renderPlotly({
+    
+    comorbid_widget <- chart2 %>% 
+      filter(disorder_names %in% input$comorbid_selection)
+    
+    emplot <- plot_ly(comorbid_widget, x = ~disorder_names, y = ~just_cp, type = 'bar', name = 'High Chronic Pain') %>% 
+      add_trace(y = ~just_op, name = 'Opioid Use') %>%
+      add_trace(y = ~just_sf, name = 'Difficulty with Social Functioning') %>%
+      add_trace(y = ~just_hy, name = 'Hypertension') %>%
+      layout(title = "Frequency of Mental Health Comorbidities in the 2020 NHIS",
+             yaxis = list(title = 'Number of Respondents'),
+             xaxis = list(title = 'Mental Disorder'))
+    return(emplot)
+    
+  })
+  
   output$plot3 <- renderPlotly({
     
     filtered_main <- chart3 %>% 
